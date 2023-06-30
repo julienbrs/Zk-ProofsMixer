0e023eb4104a14d541bf8f0cfb590c40865b5bde
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-07-01 01:41:27 +0200

    remove useless files

---
+++ contracts/src/Authentification.test.ts
@@ -0,0 +1,7 @@
+// import { Authentification.ts } from './Authentification.ts';
+
+describe('Authentification.ts.js', () => {
+  describe('Authentification.ts()', () => {
+    it.todo('should be correct');
+  });
+});
