deda3edd52f0b86842675588b1842a884b12c5f1
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-07-02 22:17:45 +0200

    deposit function, should handle amount + send

--- contracts/src/zkMixer.ts
+++ contracts/src/zkMixer.ts
@@ -16,4 +16,22 @@ export class zkMixer extends SmartContract {
   @method initState(initialRoot: Field) {
     this.mapRoot.set(initialRoot);
   }
+
+  deposit(amount: Field, commitment: Field, witness: MerkleMapWitness) {
+    this.mapRoot.getAndAssertEquals();
+
+    const notDeposited = Field(0);
+    const [oldRoot, key] = witness.computeRootAndKey(notDeposited);
+    oldRoot.assertEquals(this.mapRoot.get());
+
+    key.assertEquals(commitment);
+
+    // compute the root after incrementing
+    const deposited = Field(1); // Find a way to put the amount also here
+    const [newRoot, _] = witness.computeRootAndKey(deposited);
+
+    this.mapRoot.set(newRoot);
+
+    // How to send the amount to the user?
+  }
 }
