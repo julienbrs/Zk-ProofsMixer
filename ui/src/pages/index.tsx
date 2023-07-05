54acc1fccf012f093e1183087b72d99b059f7692
julio4 <jules.doumeche@gmail.com>
2023-07-05 11:30:13 +0200

    feat: add minimalist wip ui

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -7,10 +7,15 @@ import styles from '../styles/Home.module.css';
 import { useEffect, useState } from "react";
 
 import ZkappWorkerClient from './zkappWorkerClient';
-import { PublicKey, Field } from 'snarkyjs';
+import { PublicKey, Field, UInt32 } from 'snarkyjs';
 
 const CONTRACT_ADDRESS = 'B62qjKo8hYqsdjw68hSBB9XaURdNa2JQGi63yLDfVDAx21cnSdEfBeP';
 let transactionFee = 0.1;
+
+type DepositNote = {
+  depositNonce: UInt32,
+  nullifier: Field,
+}
 
 export default function Home() {
   let [state, setState] = useState({
@@ -20,10 +25,17 @@ export default function Home() {
     accountExists: false,
     publicKey: null as null | PublicKey,
     zkappPublicKey: null as null | PublicKey,
-    creatingTransaction: false,
+    creatingDepositTransaction: false,
+    creatingWithdrawTransaction: false,
     currentCommitmentsRoot: null as null | Field,
     currentNullifierHashesRoot: null as null | Field,
   });
+  let [input, setInput] = useState({
+    depositType: 0 | 1 | 2,
+    addressToWithdraw: null as null | PublicKey,
+    withdrawNote: '',
+  });
+  let [depositNote, setDepositNote] = useState<null | DepositNote>(null)
 
   // SETUP
   useEffect(() => {
@@ -108,21 +120,46 @@ export default function Home() {
   }, [state.hasBeenSetup]);
 
   // -------------------------------------------------------
-  // Send a deposit transaction
+  // Transaction handlersing
+
 
   const onSendDepositTransaction = async () => {
-    setState({ ...state, creatingTransaction: true });
+    setState({ ...state, creatingDepositTransaction: true });
     console.log("sending a deposit transaction...");
 
-    await state.zkappWorkerClient!.fetchAccount({
+    const caller = await state.zkappWorkerClient!.fetchAccount({
       publicKey: state.publicKey!,
     });
 
+    if (caller.error != null) {
+      throw new Error("No account");
+    }
+
+    // get caller's information
+    const depositNonce = caller.account.nonce;
+    const nullifier = Field.random();
+    const commitment = Field.random(); // TODO
+
+    // calculate the new commitment. If `addressToWithdraw` is Field(0), then it won't change the hash
+    // and the deposit will be withdrawable to any address that got the note
+    // const newCommitment = Poseidon.hash(
+    //   [
+    //     depositNonce.toFields(),
+    //     nullifier,
+    //     depositType,
+    //     addressToWithdraw,
+    //   ].flat()
+    // );
+
+    // get the witness for the current tree
+    // const commitmentWitness = commitmentMap.getWitness(newCommitment);
+    const commitmentWitness = Field.random(); // TODO
+
+    // on-chain deposit
     await state.zkappWorkerClient!.createDepositTransaction(
-      // TODO: get this from the UI
-      Field.random(), // commitment
-      Field.random(), // witness
-      Field(1)        // deposit type
+      commitment,
+      commitmentWitness,
+      Field(input.depositType)
     );
 
     console.log("creating proof...");
@@ -144,11 +181,20 @@ export default function Home() {
       "See transaction at https://berkeley.minaexplorer.com/transaction/" + hash
     );
 
-    setState({ ...state, creatingTransaction: false });
+    // update the leaf locally if the deposit was successful
+    // commitmentMap.set(newCommitment, depositType);
+
+    // save necessary information for withdrawal
+    setDepositNote({
+      depositNonce,
+      nullifier,
+    })
+
+    setState({ ...state, creatingDepositTransaction: false });
   };
 
   const onSendWithdrawTransaction = async () => {
-    setState({ ...state, creatingTransaction: true });
+    setState({ ...state, creatingWithdrawTransaction: true });
     console.log("sending a withdraw transaction...");
 
     await state.zkappWorkerClient!.fetchAccount({
@@ -184,7 +230,7 @@ export default function Home() {
       "See transaction at https://berkeley.minaexplorer.com/transaction/" + hash
     );
 
-    setState({ ...state, creatingTransaction: false });
+    setState({ ...state, creatingWithdrawTransaction: false });
   };
 
   // -------------------------------------------------------
@@ -228,7 +274,7 @@ export default function Home() {
 
   let setupText = state.hasBeenSetup
     ? 'SnarkyJS Ready'
-    : 'Setting up SnarkyJS...';
+    : 'Setting up SnarkyJS... (can take up to a minute)';
   let setup = (
     <div>
       {' '}
@@ -255,32 +301,135 @@ export default function Home() {
   if (state.hasBeenSetup && state.accountExists) {
     mainContent = (
       <div>
-        <button
-          onClick={onSendDepositTransaction}
-          disabled={state.creatingTransaction}
-        >
-          {' '}
-          Deposit Transaction (unimplemented, missing args){' '}
-        </button>
-        <button
-          onClick={onSendDepositTransaction}
-          disabled={state.creatingTransaction}
-        >
-          {' '}
-          Withdraw Transaction (unimplemented, missing args){' '}
-        </button>
-        <div> Current commitmentsRoot in zkApp: {state.currentCommitmentsRoot!.toString()} </div>
-        <div> Current nullifierHashesRoot in zkApp: {state.currentNullifierHashesRoot!.toString()} </div>
-        <button onClick={onRefresh}> Get Latest State</button>
+        <div>
+          <h2>zkApp State</h2>
+          <div>Current commitmentsRoot: {state.currentCommitmentsRoot!.toString()}</div>
+          <div>Current nullifierHashesRoot: {state.currentNullifierHashesRoot!.toString()}</div>
+          <button onClick={onRefresh}>Get Latest State</button>
+        </div>
+
+        <div>
+          <h2>Deposit</h2>
+
+          <div>
+            <input
+              type="radio"
+              id="depositType1"
+              name="depositType"
+              value="0"
+              checked={input.depositType === 0}
+              onChange={(e) => {
+                setInput({ ...input, depositType: parseInt(e.target.value) });
+              }}
+            />
+            <label htmlFor="depositType1"> Deposit 1 MINA </label>
+
+            <input
+              type="radio"
+              id="depositType2"
+              name="depositType"
+              value="1"
+              checked={input.depositType === 1}
+              onChange={(e) => {
+                setInput({ ...input, depositType: parseInt(e.target.value) });
+              }}
+            />
+            <label htmlFor="depositType2"> Deposit 5 MINA </label>
+
+            <input
+              type="radio"
+              id="depositType3"
+              name="depositType"
+              value="2"
+              checked={input.depositType === 2}
+              onChange={(e) => {
+                setInput({ ...input, depositType: parseInt(e.target.value) });
+              }}
+            />
+            <label htmlFor="depositType3"> Deposit 10 MINA ?</label>
+          </div>
+
+          {/* TODO: custom target address */}
+
+          <button
+            onClick={onSendDepositTransaction}
+            disabled={state.creatingDepositTransaction}
+          >
+            {' '}
+            Deposit Transaction{' '}
+          </button>
+
+          {depositNote &&
+            <div>
+              <h3>Deposit Note</h3>
+              <p>Please save this note. You will need it to withdraw your funds.</p>
+              <textarea
+                value={depositNote.depositNonce.toString().concat(depositNote.nullifier.toString())}
+                readOnly={true}
+                onClick={(e) => {
+                  (e.target as HTMLTextAreaElement).select();
+                  document.execCommand('copy');
+                }}
+              />
+            </div>
+          }
+        </div>
+
+        <div>
+          <h2>Withdraw</h2>
+
+          <input
+            type="text"
+            placeholder="Deposit Note"
+            value={input.withdrawNote}
+            onChange={(e) => {
+              setInput({ ...input, withdrawNote: e.target.value });
+            }}
+          />
+
+          <button
+            onClick={onSendWithdrawTransaction}
+            disabled={state.creatingWithdrawTransaction}
+          >
+            {' '}
+            Withdraw Transaction{' '}
+          </button>
+        </div>
       </div>
     );
   }
 
-  return (
+  const debug = (
     <div>
-      {setup}
-      {accountDoesNotExist}
-      {mainContent}
+      <h2>Debug</h2>
+      <code>
+        <span>state:</span>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
+
+        <span>input:</span>
+        <pre>{JSON.stringify(input, null, 2)}</pre>
+
+        <span>depositNote:</span>
+        <pre>{JSON.stringify(depositNote, null, 2)}</pre>
+      </code>
+    </div>
+  )
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+        <div>
+          {setup}
+          {accountDoesNotExist}
+          {mainContent}
+        </div>
+
+        <div>
+          {debug}
+        </div>
+
+      </div>
     </div>
   );
 
